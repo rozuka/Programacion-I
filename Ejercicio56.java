18ed06f8343587d6a8b5535d2bf0c5c076d70a72
rozuka <150718753+rozuka@users.noreply.github.com>
2024-09-30 20:38:16 +0000

    Ejercicio otra vez

--- Ejercicio56.java
+++ Ejercicio56.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Ejercicio56{
     public static void main( String args[]){
         
-        System.out.println("hola como estas");
+        Scanner leer = new Scanner (System.in);
+        int aprobados= 0;
+        int reprobados= 0;
+        System.out.println("digite la cantidad de estudiantes");
+
+        int estudiantes = leer.nextInt();
+    
+
+        for (int i=0; i <= estudiantes; i++);
+        System.out.print("ingrese las notas de el estudiante: ");
+
+        int not = leer.nextInt();
+
+        if(not>=5){
+            aprobados++;
+        }else{
+            reprobados++;
+        }
+        System.out.println("los aprobados son "+ aprobados + " y los reprobados son "+ reprobados);
     }
 }
